8100f50e1d7672b3beb527270a799357c4f14216
GoofyG00ber <czanik.csanad@gmail.com>
2024-05-15 11:04:46 +0200

    Add project files.

---
+++ zh3_gyak/UserControl1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace zh3_gyak
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(25, 48);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(174, 244);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(25, 15);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(219, 65);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 273);
+            dataGridView1.TabIndex = 2;
+            // 
+            // UserControl1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "UserControl1";
+            Size = new Size(542, 384);
+            Load += UserControl1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ zh3_gyak/UserControl1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using zh3_gyak.Models;
+
+namespace zh3_gyak
+{
+    public partial class UserControl1 : UserControl
+    {
+        StudiesContext context = new StudiesContext();
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            var er = from x in context.Instructors
+                     select x;
+
+            listBox1.DataSource = er.ToList();
+            listBox1.DisplayMember = "Name";
+
+            var oktatóSzám = context.Instructors.Count();
+            var legmagasabbStatus = (from x in context.Instructors
+                                     select x.StatusFk).Max();
+
+            var legrangosabb = (from x in context.Instructors
+                                      where x.StatusFk == legmagasabbStatus
+                                      select x.Name);
+
+            MessageBox.Show($"{string.Join(',', legrangosabb)}");
+
+            //csv-be iras feladat lehet
+
+            // uj terem felvétele adatbazisba feladat lehet
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null) return;
+
+            var instructor = (Instructor)listBox1.SelectedItem;
+
+            var órák = from x in context.Lessons
+                       where x.InstructorFK == instructor.InstructorSk///////////////
+                       select new
+                       {
+                           x.CourseFkNavigation.Name,
+                           x.CourseFkNavigation.Code,
+                           Nap = x.DayFkNavigation.Name,
+                           Sáv = x.TimeFkNavigation.Name,
+                           Terem = x.RoomFkNavigation.Name,
+                       };
+
+            dataGridView1.DataSource = órák.toList();/////////
+        }
+    }
+}
